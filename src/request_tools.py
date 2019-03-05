46ea289a15cbaef9f1be850fe2be11661b24d516
bentondrew <benton.s.drew@drewantech.com>
2019-03-05 15:03:07 -0600

    Actually got application json back from challenge service so changing expected content type.

--- src/request_tools.py
+++ src/request_tools.py
@@ -6,18 +6,14 @@ def get_json_request(url):
     Makes a get request to the provided url.
 
     Raises Runtime errors if the HTTP status code is
-    not 200 or the content-type is not text/html.
-        Note: URL for the tests actually return json
-              data types but the server implements the
-              content type as text/html when it should
-              be application/json.
+    not 200 or the content-type is not application/json.
 
     Returns the json data in the body encoded in python
     data objects.
     """
     resp = requests.get(url)
     if resp.status_code == 200:
-        if 'text/html' in resp.headers['content-type']:
+        if 'application/json' in resp.headers['content-type']:
             return resp.json()
         else:
             raise RuntimeError('Expected html content type '
