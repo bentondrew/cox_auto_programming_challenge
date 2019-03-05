f8b33c441e2c5a67d36b601e32e11a97f89c00bb
bentondrew <benton.s.drew@drewantech.com>
2019-03-05 17:15:26 -0600

    Add collecting list of vehicle ids.

--- src/data_collection.py
+++ src/data_collection.py
@@ -10,22 +10,60 @@ def get_dataset_id():
     Does not catch exceptions.
 
     Raises RunimeError if data back from url is not a dict
-    with a string value. Explicitly attempts to access
-    'datasetId' key to raise KeyError if key doesn't exist.
+    with a string value for the 'datasetId' key. Explicitly
+    attempts to access 'datasetId' key to raise KeyError if
+    key doesn't exist.
 
     Returns the sting value for the 'datasetId' key in the
     received dict.
     """
     url = 'https://vautointerview.azurewebsites.net/api/datasetid'
-    data_set_id = get_json_request(url=url)
-    if type(data_set_id) is not dict:
+    data_set_dict = get_json_request(url=url)
+    if type(data_set_dict) is not dict:
         raise RuntimeError('Data returned {} from {} is not of type '
-                           'dict.'.format(data_set_id, url))
+                           'dict.'.format(data_set_dict, url))
     # Check if key in dict; expected to raise KeyError if key
     # not in dict.
-    value = data_set_id['datasetId']
+    value = data_set_dict['datasetId']
     if type(value) is not str:
         raise RuntimeError('Data returned {} from {} does not have value '
                            'of type str for key datasetId.'
-                           .format(data_set_id, url))
+                           .format(data_set_dict, url))
+    return value
+
+
+def get_vehicle_ids(data_set_id):
+    """
+    Makes a request to the
+    https://vautointerview.azurewebsites.net/api/{datasetId}/vehicles
+    url to get a list of vehicle ids.
+
+    Does not catch exceptions.
+
+    Raises RunimeError if data back from url is not a dict
+    with a list value for the 'vehicleIds' key. The list
+    is expected to be a list of integers. Explicitly
+    attempts to access 'vehicleIds' key to raise KeyError if
+    key doesn't exist.
+
+    Returns a list of the vehicle ids.
+    """
+    url = 'https://vautointerview.azurewebsites.net/api/{}/vehicles'
+    vehicle_id_dict = get_json_request(url=url)
+    if type(vehicle_id_dict) is not dict:
+        raise RuntimeError('Data returned {} from {} is not of type '
+                           'dict.'.format(vehicle_id_dict, url))
+    # Check if key in dict; expected to raise KeyError if key
+    # not in dict.
+    value = vehicle_id_dict['vehicleIds']
+    if type(value) is not list:
+        raise RuntimeError('Data returned {} from {} does not have value '
+                           'of type list for key vehicleIds.'
+                           .format(vehicle_id_dict, url))
+    for i, v_id in enumerate(value):
+        if type(v_id) is not int:
+            raise RuntimeError('Data {} at index {} in list of vehicle ids {} '
+                               'returned from {} does not have value '
+                               'of type int.'
+                               .format(v_id, i, value, url))
     return value
--- tests/data_collection_test.py
+++ tests/data_collection_test.py
@@ -1,6 +1,7 @@
 import mock
 import pytest
-from cox_auto_app.data_collection import (get_dataset_id)
+from cox_auto_app.data_collection import (get_dataset_id,
+                                          get_vehicle_ids)
 
 
 class TestGetDatasetid(object):
@@ -64,3 +65,91 @@ class TestGetDatasetid(object):
         json_data = {'datasetId': 'test'}
         mock_get.return_value = json_data
         assert get_dataset_id() == json_data['datasetId']
+
+
+class TestGetVehicleids(object):
+    """
+    Tests for get_vehicle_ids function.
+    """
+    # get_json_request pass through tests.
+    @mock.patch('cox_auto_app.data_collection.get_json_request')
+    def test_bad_status(self, mock_get):
+        data_set_id = '7'
+        url = ('https://vautointerview.azurewebsites.net/api/{}/vehicles'
+               .format(data_set_id))
+        return_status = 500
+        expected_error = ('Got unexpected status code {} from url '
+                          '{}'.format(return_status, url))
+        mock_get.side_effect = RuntimeError(expected_error)
+        with pytest.raises(RuntimeError, match=expected_error):
+            get_vehicle_ids(data_set_id=data_set_id)
+
+    @mock.patch('cox_auto_app.data_collection.get_json_request')
+    def test_bad_content(self, mock_get):
+        data_set_id = '7'
+        url = ('https://vautointerview.azurewebsites.net/api/{}/vehicles'
+               .format(data_set_id))
+        return_content = 'bad'
+        expected_error = ('Expected html content type '
+                          'from url {} but got {}'
+                          .format(url, return_content))
+        mock_get.side_effect = RuntimeError(expected_error)
+        with pytest.raises(RuntimeError, match=expected_error):
+            get_vehicle_ids(data_set_id=data_set_id)
+
+    # get_vehicle_ids generated exceptions
+    @mock.patch('cox_auto_app.data_collection.get_json_request')
+    def test_return_not_dict(self, mock_get):
+        data_set_id = '7'
+        url = ('https://vautointerview.azurewebsites.net/api/{}/vehicles'
+               .format(data_set_id))
+        json_data = 1
+        expected_error = ('Data returned {} from {} is not of type '
+                          'dict.'.format(json_data, url))
+        mock_get.return_value = json_data
+        with pytest.raises(RuntimeError, match=expected_error):
+            get_vehicle_ids(data_set_id=data_set_id)
+
+    @mock.patch('cox_auto_app.data_collection.get_json_request')
+    def test_key_not_in_return(self, mock_get):
+        data_set_id = '7'
+        json_data = {'test': True}
+        mock_get.return_value = json_data
+        with pytest.raises(KeyError):
+            get_vehicle_ids(data_set_id=data_set_id)
+
+    @mock.patch('cox_auto_app.data_collection.get_json_request')
+    def test_return_value_for_key_not_str(self, mock_get):
+        data_set_id = '7'
+        url = ('https://vautointerview.azurewebsites.net/api/{}/vehicles'
+               .format(data_set_id))
+        json_data = {'vehicleIds': True}
+        expected_error = ('Data returned {} from {} does not have value '
+                          'of type list for key vehicleIds.'
+                          .format(json_data, url))
+        mock_get.return_value = json_data
+        with pytest.raises(RuntimeError, match=expected_error):
+            get_vehicle_ids(data_set_id=data_set_id)
+
+    @mock.patch('cox_auto_app.data_collection.get_json_request')
+    def test_value_in_list_not_int(self, mock_get):
+        data_set_id = '7'
+        url = ('https://vautointerview.azurewebsites.net/api/{}/vehicles'
+               .format(data_set_id))
+        json_data = {'vehicleIds': [1, 'hi']}
+        expected_error = ('Data {} at index {} in list of vehicle ids {} '
+                          'returned from {} does not have value '
+                          'of type int.'
+                          .format('hi', 1, json_data['vehicleIds'], url))
+        mock_get.return_value = json_data
+        with pytest.raises(RuntimeError, match=expected_error):
+            get_vehicle_ids(data_set_id=data_set_id)
+
+    # good test
+    @mock.patch('cox_auto_app.data_collection.get_json_request')
+    def test_good_return(self, mock_get):
+        data_set_id = '7'
+        json_data = {'vehicleIds': [1, 2]}
+        mock_get.return_value = json_data
+        assert get_vehicle_ids(
+            data_set_id=data_set_id) == json_data['vehicleIds']
