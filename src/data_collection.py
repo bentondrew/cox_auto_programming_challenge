617e15bb52ed7dddf9629d58d282c88c512ede60
bentondrew <benton.s.drew@drewantech.com>
2019-03-05 16:49:44 -0600

    Change datasetid dict key check to datasetId

--- src/data_collection.py
+++ src/data_collection.py
@@ -5,13 +5,13 @@ def get_dataset_id():
     """
     Makes a request to the
     https://vautointerview.azurewebsites.net/api/datasetid
-    url to get a datasetid.
+    url to get a datasetId.
 
     Does not catch exceptions.
 
     Raises RunimeError if data back from url is not a dict
     with a string value. Explicitly attempts to access
-    'datasetid' key to raise KeyError if key doesn't exist.
+    'datasetId' key to raise KeyError if key doesn't exist.
 
     Returns the sting value for the 'datasetId' key in the
     received dict.
@@ -23,9 +23,9 @@ def get_dataset_id():
                            'dict.'.format(data_set_id, url))
     # Check if key in dict; expected to raise KeyError if key
     # not in dict.
-    value = data_set_id['datasetid']
+    value = data_set_id['datasetId']
     if type(value) is not str:
         raise RuntimeError('Data returned {} from {} does not have value '
-                           'of type str for key datasetid.'
+                           'of type str for key datasetId.'
                            .format(data_set_id, url))
     return value
