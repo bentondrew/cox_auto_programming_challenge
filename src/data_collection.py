d4a46065f46fb11fda7b628c6fdf1a56e5a016e4
bentondrew <benton.s.drew@drewantech.com>
2019-03-05 16:06:25 -0600

    Add more detail to error message.

--- src/data_collection.py
+++ src/data_collection.py
@@ -19,12 +19,13 @@ def get_dataset_id():
     url = 'https://vautointerview.azurewebsites.net/api/datasetid'
     data_set_id = get_json_request(url=url)
     if data_set_id is not dict:
-        raise RuntimeError('Data returned from {} is not of type '
-                           'dict.'.format(url))
+        raise RuntimeError('Data returned {} from {} is not of type '
+                           'dict.'.format(data_set_id, url))
     # Check if key in dict; expected to raise KeyError if key
     # not in dict.
     value = data_set_id['datasetid']
     if value is not str:
-        raise RuntimeError('Data returned from {} does not have value '
-                           'of type str for key datasetid.'.format(url))
+        raise RuntimeError('Data returned {} from {} does not have value '
+                           'of type str for key datasetid.'
+                           .format(data_set_id, url))
     return data_set_id
