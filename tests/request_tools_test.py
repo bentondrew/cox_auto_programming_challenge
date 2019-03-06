f387ebc4196cc5010a473c90fd34fafbdc29f0b5
bentondrew <benton.s.drew@drewantech.com>
2019-03-06 11:41:41 -0600

    Add test for request tools check response.

--- tests/request_tools_test.py
+++ tests/request_tools_test.py
@@ -1,7 +1,8 @@
 import mock
 import pytest
 from cox_auto_app.request_tools import (get_json_request,
-                                        post_json_request)
+                                        post_json_request,
+                                        check_response)
 
 
 class TestGetJson(object):
@@ -96,6 +97,43 @@ class TestPostJson(object):
         mock_post.return_value.json.return_value = json_data
         mock_check.return_value = json_data
         assert post_json_request(url=url, post_data=json_data) == json_data
+
+
+class TestCheckResponse(object):
+    """
+    Tests for check_response function.
+    """
+    def test_bad_status(self):
+        url = 'https://vautointerview.azurewebsites.net/api/datasetid'
+        return_status = 500
+        mock_response = mock.MagicMock()
+        mock_response.return_value.status_code = return_status
+        expected_error = ('Got unexpected status code {} from url '
+                          '{}'.format(return_status, url))
+        with pytest.raises(RuntimeError, match=expected_error):
+            check_response(url=url, response=mock_response)
+
+    def test_bad_content(self):
+        url = 'https://vautointerview.azurewebsites.net/api/datasetid'
+        return_content = 'bad'
+        mock_response = mock.MagicMock()
+        mock_response.return_value.status_code = 200
+        mock_response.return_value.headers = {'content-type': return_content}
+        expected_error = ('Expected json content type '
+                          'from url {} but got {}'
+                          .format(url, return_content))
+        with pytest.raises(RuntimeError, match=expected_error):
+            check_response(url=url, response=mock_response)
+
+    def test_good_return(self):
+        url = 'https://vautointerview.azurewebsites.net/api/datasetid'
+        return_content = 'application/json'
+        json_data = {'test': True}
+        mock_response = mock.MagicMock()
+        mock_response.return_value.status_code = 200
+        mock_response.return_value.headers = {'content-type': return_content}
+        mock_response.return_value.json.return_value = json_data
+        assert check_response(url=url, response=mock_response) == json_data
 
     #    @mock.patch('requests.get')
     #    def test_good_return(self, mock_get):
