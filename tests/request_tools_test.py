59defbe8b2d2fea8eddf70d6f06d9567b813327f
bentondrew <benton.s.drew@drewantech.com>
2019-03-06 11:29:13 -0600

    Add json post tests.

--- tests/request_tools_test.py
+++ tests/request_tools_test.py
@@ -1,6 +1,7 @@
 import mock
 import pytest
-from cox_auto_app.request_tools import (get_json_request)
+from cox_auto_app.request_tools import (get_json_request,
+                                        post_json_request)
 
 
 class TestGetJson(object):
@@ -47,6 +48,54 @@ class TestGetJson(object):
         mock_get.return_value.json.return_value = json_data
         mock_check.return_value = json_data
         assert get_json_request(url=url) == json_data
+
+
+class TestPostJson(object):
+    """
+    Tests for post_json_request function. These tests demonstrate
+    expected failure modes for post_json_request function in
+    addition to testing the calling of this function by the
+    merge function.
+    """
+    @mock.patch('cox_auto_app.request_tools.check_response')
+    @mock.patch('requests.post')
+    def test_bad_status(self, mock_post, mock_check):
+        url = 'https://vautointerview.azurewebsites.net/api/datasetid'
+        return_status = 500
+        mock_post.return_value.status_code = return_status
+        expected_error = ('Got unexpected status code {} from url '
+                          '{}'.format(return_status, url))
+        mock_check.side_effect = RuntimeError(expected_error)
+        post_data = {'test': True}
+        with pytest.raises(RuntimeError, match=expected_error):
+            post_json_request(url=url, post_data=post_data)
+
+    @mock.patch('cox_auto_app.request_tools.check_response')
+    @mock.patch('requests.post')
+    def test_bad_content(self, mock_post, mock_check):
+        url = 'https://vautointerview.azurewebsites.net/api/datasetid'
+        return_content = 'bad'
+        mock_post.return_value.status_code = 200
+        mock_post.return_value.headers = {'content-type': return_content}
+        expected_error = ('Expected json content type '
+                          'from url {} but got {}'
+                          .format(url, return_content))
+        mock_check.side_effect = RuntimeError(expected_error)
+        post_data = {'test': True}
+        with pytest.raises(RuntimeError, match=expected_error):
+            post_json_request(url=url, post_data=post_data)
+
+    @mock.patch('cox_auto_app.request_tools.check_response')
+    @mock.patch('requests.post')
+    def test_good_return(self, mock_post, mock_check):
+        url = 'https://vautointerview.azurewebsites.net/api/datasetid'
+        return_content = 'application/json'
+        json_data = {'test': True}
+        mock_post.return_value.status_code = 200
+        mock_post.return_value.headers = {'content-type': return_content}
+        mock_post.return_value.json.return_value = json_data
+        mock_check.return_value = json_data
+        assert post_json_request(url=url, post_data=json_data) == json_data
 
     #    @mock.patch('requests.get')
     #    def test_good_return(self, mock_get):
